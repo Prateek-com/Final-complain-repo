113d54c6e9b4ad66b1e6014cf9df7e119cffcc32
PRATEEK CHAUDHARY <prateek9q9q@gmail.com>
2026-04-13 22:07:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -247,4 +247,5 @@ document.getElementById('compliaint-modal').addEventListener('click',e => {
 <div class="modal-field"><span class="modal-label">Mobile</span><span class="modal-value">${complaint.mobile}</span></div>
 <div class="modal-field"><span class="modal-label">Category</span><span class="modal-value">${complaint.category}</span></div>
 <div class="modal-field"><span class="modal-label">Area / Ward</span><span class="modal-value">${complaint.area}</span></div>
+<div class="modal-field"><span class="modal-label">Description</span><span class="modal-value">${complaint.description}</span></div>
 
