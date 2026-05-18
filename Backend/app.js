6c93767b73086a3eaf1d1114c518882b3ee18df2
PRATEEK CHAUDHARY <prateek9q9q@gmail.com>
2026-05-18 21:12:29 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -367,3 +367,4 @@ noMsg.classList.add('hidden');
           <td>
             <select data-index="${origIdx}" class="status-select">
              <option value="SUBMITTED" ${c.status === 'SUBMITTED' ? 'selected' : ''}>Submitted</option>
+              <option value="IN_PROGRESS" ${c.status === 'IN_PROGRESS' ? 'selected' : ''}>In Progress</option>
